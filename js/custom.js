f6d417ffdd22f858d714d1e0abd8d85484cda9d4
unknown <samuelsc2518@gmail.com>
2021-11-22 17:47:11 -0400

    poniendo hora de prueba

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 Theme by: WebThemez.com 
 */
 $( function() {
-        var endDate = "December  28, 2021 15:03:25";
+        var endDate = "November  22, 2021 17:50:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
